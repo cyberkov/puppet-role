00a882aed48fcc3c3ad47244e6711463441ba067
Kristof Willaert <kristof.willaert@gmail.com>
2012-10-19 17:58:33 +0200

    Replace all occurrences of old nodenames with platform name in tests.

--- spec/acceptance/oracleprereq/limits_spec.rb
+++ spec/acceptance/oracleprereq/limits_spec.rb
@@ -8,7 +8,7 @@ describe 'Default node for oracle' do
       limits_entry = "oracle soft nofile 1024"
 
       it "should contain #{limits_entry}" do
-      	return_code = @env.vms[:puppetnode2].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
+      	return_code = @env.vms[:centos5].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
       	return_code.should eql(0)
       end
     end
@@ -17,7 +17,7 @@ describe 'Default node for oracle' do
       limits_entry = "oracle hard nofile 65536"
 
       it "should contain #{limits_entry}" do
-        return_code = @env.vms[:puppetnode2].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
+        return_code = @env.vms[:centos5].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
         return_code.should eql(0)
       end
     end
@@ -26,7 +26,7 @@ describe 'Default node for oracle' do
       limits_entry = "oracle soft nproc 2047"
 
       it "should contain #{limits_entry}" do
-        return_code = @env.vms[:puppetnode2].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
+        return_code = @env.vms[:centos5].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
         return_code.should eql(0)
       end
     end
@@ -35,7 +35,7 @@ describe 'Default node for oracle' do
       limits_entry = "oracle hard nproc 16384"
 
       it "should contain #{limits_entry}" do
-        return_code = @env.vms[:puppetnode2].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
+        return_code = @env.vms[:centos5].channel.sudo("grep '#{limits_entry}' /etc/security/limits.conf 2> /dev/null 1> /dev/null")
         return_code.should eql(0)
       end
     end
