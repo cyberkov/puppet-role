464fe43e76736df0fe13fab87cc0f68bab6d185f
Kristof Willaert <kristof.willaert@gmail.com>
2012-10-24 00:20:23 +0200

    Cleanup manifests to get rid of puppet-lint warnings.

--- manifests/oracleprereq.pp
+++ manifests/oracleprereq.pp
@@ -16,11 +16,40 @@ class role::oracleprereq {
   Users::Localgroup <| title == 'dba' |>
   Users::Localuser <| title == 'oracle' |>
 
-  limits::conf { 'oracle-soft-nofile': domain => 'oracle', type => 'soft', item => 'nofile', value => '1024' }
-  limits::conf { 'oracle-hard-nofile': domain => 'oracle', type => 'hard', item => 'nofile', value => '65536' }
-  limits::conf { 'oracle-soft-nproc': domain  => 'oracle', type => 'soft', item => 'nproc', value  => '2047' }
-  limits::conf { 'oracle-hard-nproc': domain  => 'oracle', type => 'hard', item => 'nproc', value  => '16384' }
-  limits::conf { 'oracle-hard-stack': domain  => 'oracle', type => 'hard', item => 'stack', value  => '10240' }
+  limits::conf { 'oracle-soft-nofile':
+    domain => 'oracle',
+    type   => 'soft',
+    item   => 'nofile',
+    value  => '1024'
+  }
+
+  limits::conf { 'oracle-hard-nofile':
+    domain => 'oracle',
+    type   => 'hard',
+    item   => 'nofile',
+    value  => '65536'
+  }
+
+  limits::conf { 'oracle-soft-nproc':
+    domain => 'oracle',
+    type   => 'soft',
+    item   => 'nproc',
+    value  => '2047'
+  }
+
+  limits::conf { 'oracle-hard-nproc':
+    domain => 'oracle',
+    type   => 'hard',
+    item   => 'nproc',
+    value  => '16384'
+  }
+
+  limits::conf { 'oracle-hard-stack':
+    domain => 'oracle',
+    type   => 'hard',
+    item   => 'stack',
+    value  => '10240'
+  }
 
   class { 'ntp':
     ntp_servers => ['tik.cegeka.be', 'tak.cegeka.be'],
